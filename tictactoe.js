22136dd7f98cb94c2f799e79253e66091908f598
#1 clairo fan <peaceforever231@gmail.com>
2025-11-29 23:54:53 -0500

    yes

--- tictactoe.js
+++ tictactoe.js
@@ -11,23 +11,17 @@ const gameStatus = {
 window.addEventListener("DOMContentLoaded", domLoaded);
 
 function domLoaded() {
-	// Setup the click event for the "New game" button
 	const newBtn = document.getElementById("newGameButton");
 	newBtn.addEventListener("click", newGame);
 
-	// Create click-event handlers for each game board button
 	const buttons = getGameBoardButtons();
 	for (let button of buttons) {
 		button.addEventListener("click", function () { boardButtonClicked(button); });
 	}
 
-	// Clear the board
 	newGame();
 }
 
-// Returns an array of 9 <button> elements that make up the game board. The first 3 
-// elements are the top row, the next 3 the middle row, and the last 3 the 
-// bottom row. 
 function getGameBoardButtons() {
 	return document.querySelectorAll("#gameBoard > button");
 }
@@ -36,20 +30,17 @@ function checkForWinner() {
 	
 	const buttons = getGameBoardButtons();
 
-	// Ways to win
 	const possibilities = [
-		[0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
-		[0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
-		[0, 4, 8], [2, 4, 6] // diagonals
+		[0, 1, 2], [3, 4, 5], [6, 7, 8], 
+		[0, 3, 6], [1, 4, 7], [2, 5, 8], 
+		[0, 4, 8], [2, 4, 6] 
 	];
 
-	// Check for a winner first
 	for (let indices of possibilities) {
 		if (buttons[indices[0]].innerHTML !== "" &&
 			buttons[indices[0]].innerHTML === buttons[indices[1]].innerHTML &&
 			buttons[indices[1]].innerHTML === buttons[indices[2]].innerHTML) {
 			
-			// Found a winner
 			if (buttons[indices[0]].innerHTML === "X") {
 				return gameStatus.HUMAN_WINS;
 			}
@@ -59,19 +50,16 @@ function checkForWinner() {
 		}
 	}
 
-	// See if any more moves are left
 	for (let button of buttons) {
 		if (button.innerHTML !== "X" && button.innerHTML !== "O") {
 			return gameStatus.MORE_MOVES_LEFT;
 		}
 	}
 
-	// If no winner and no moves left, then it's a draw
 	return gameStatus.DRAW_GAME;
 }
 
 function newGame() {
-	// TODO: Complete the function
    clearTimeout(computerMoveTimeout);
    computerMoveTimeout = 0;
    const buttons = getGameBoardButtons();
@@ -86,7 +74,6 @@ function newGame() {
 }
 
 function boardButtonClicked(button) {
-	// TODO: Complete the function
    if(playerTurn === true){
       button.textContent = 'X';
       button.classList.add("x");
@@ -100,7 +87,7 @@ function switchTurn() {
    const turnInfo = document.getElementById('turnInfo'); 
 
    if (status === gameStatus.MORE_MOVES_LEFT) {
-      playerTurn = !playerTurn; // Toggle turn first
+      playerTurn = !playerTurn; 
 
       if (playerTurn) {
          turnInfo.textContent = "Your turn";
@@ -123,7 +110,6 @@ function switchTurn() {
 
 
 function makeComputerMove() {
-	// TODO: Complete the function
    const buttons = getGameBoardButtons();
    const arr = [];
    for(const button of buttons){
